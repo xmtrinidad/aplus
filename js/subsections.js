4f6a29ec9bc4b2f46e17333eab441809a5e46690
Xavier Trinidad <xmtrinidad@gmail.com>
2017-08-13 13:32:06 -0700

    Finished adding links, finished hooking up subsection buttons

--- js/subsections.js
+++ js/subsections.js
@@ -1,8 +1,11 @@
 // Show/hide individual subsections (1.1, 1.2, 1.3, etc)
 let $hwObjectives = $(".hw .objectives");
 let $nwObjectives = $(".nw .objectives");
+let $tsObjectives = $(".ts .objectives");
+
 // Hardware section
 $(".hw.section_nav p").on("click", function(){
+    // console.log($(this).index("p"));
     let hwBtnIndex0 = 0;
     let hwBtnIndex1 = 1;
     let hwBtnIndex2 = 2;
@@ -27,11 +30,12 @@ $(".hw.section_nav p").on("click", function(){
 
 // Network section
 $(".nw.section_nav p").on("click", function(){
+    // console.log($(this).index("p"));
     let nwBtnIndex19 = 19;
     let nwBtnIndex20 = 20;
 
     // All sections
-    console.log($(this).index("p"));
+
     if ($(this).index("p") === nwBtnIndex19) {
         $nwObjectives.show();
     }
@@ -42,6 +46,28 @@ $(".nw.section_nav p").on("click", function(){
     // Sections 2.5 - 2.9
     else  {
         showHide($nwObjectives, 4)
+    }
+});
+
+// Troubleshooting section
+$(".ts.section_nav p").on("click", function(){
+    console.log($(this).index("p"));
+    let tsBtnIndex37 = 37;
+    let tsBtnIndex38 = 38;
+    let tsBtnIndex39 = 39;
+
+
+    // All sections
+    if ($(this).index("p") === tsBtnIndex37) {
+        $tsObjectives.show();
+    }
+    // Sections 4.1 - 4.4
+    else if ($(this).index("p") === tsBtnIndex38) {
+        showHide($tsObjectives, 3);
+    }
+    // Sections 4.5 - 4.6
+    else  {
+        showHide($tsObjectives, 4)
     }
 });
 
